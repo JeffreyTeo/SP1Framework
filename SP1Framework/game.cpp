22363ceed869df93d3e177139304e01b5dccb8b9
Gadula <jenny_liang_leran@hotmail.com>
2014-08-20 11:55:24 +0800

    Update game.cpp

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -5,23 +5,25 @@
 #include "Framework\console.h"
 #include <iostream>
 #include <iomanip>
+#include <string>
 
+using std :: string;
+using std:: endl;
 struct System_of //Shannon : Struct Draft
 {
 	int Value;
 	COORD Location[3];
 };
-System_of Life; //Shannon : Life Struct
+System_of Life; 
 double elapsedTime;
 double deltaTime;
 bool keyPressed[K_COUNT];
 COORD charLocation;
-COORD enemyLocation[3]; //Shannon : Dummy Enemy Location
+COORD enemyLocation[3];
 COORD consoleSize;
 
-int diplayscore[10]={};
-unsigned long score =1000;//1000000000;
-
+int displayscore[10]={};
+unsigned long score = 1000;
 void init()
 {
 	// Set precision for floating point output
@@ -32,26 +34,26 @@ void init()
 
 	/* get the number of character cells in the current buffer */ 
 	GetConsoleScreenBufferInfo( GetStdHandle( STD_OUTPUT_HANDLE ), &csbi );
-	consoleSize.X = csbi.srWindow.Right +0;//- 5;
-	consoleSize.Y = csbi.srWindow.Bottom +25;//- 5;
+	consoleSize.X = csbi.srWindow.Right + 1;
+	consoleSize.Y = csbi.srWindow.Bottom;
 
 	// set the character to be in the center of the screen.
-	charLocation.X = consoleSize.X / 2;
-	charLocation.Y = consoleSize.Y / 2;
+	charLocation.X = consoleSize.X/8;
+	charLocation.Y = consoleSize.Y + 47;
 
-		// Shannon : Implement Dummy Enemy
+	// Shannon : Implement Dummy Enemy
 	for (int i = 0; i < 3; ++i)
 	{
-		enemyLocation[i].X = consoleSize.X - 2*(i+5);
-		enemyLocation[i].Y = consoleSize.Y - 2*(i+5);
+		enemyLocation[i].X = consoleSize.X - 2*(i+20);
+		enemyLocation[i].Y = consoleSize.Y - 2*(i-10);
 	}
 
 	//Shannon : Implement Life System
 	Life.Value = 3;
 	for (int i = 0; i < 3; ++i)
 	{
-		Life.Location[i].X = consoleSize.X - 3*(i+1);
-		Life.Location[i].Y = consoleSize.Y - 25;
+		Life.Location[i].X = consoleSize.X - 4*(i+2);
+		Life.Location[i].Y = consoleSize.Y-18;
 	}
 
 	elapsedTime = 0.0;
@@ -60,13 +62,12 @@ void init()
 void shutdown()
 {
 	// Reset to white text on black background
-	colour(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED);
+	colour(FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED );
 }
 
 void getInput()
 {    
-	// keyPressed[K_UP] = isKeyPressed(VK_UP);
-	//  keyPressed[K_DOWN] = isKeyPressed(VK_DOWN);
+	keyPressed[K_UP] = isKeyPressed(VK_UP);
 	keyPressed[K_LEFT] = isKeyPressed(VK_LEFT);
 	keyPressed[K_RIGHT] = isKeyPressed(VK_RIGHT);
 	keyPressed[K_ESCAPE] = isKeyPressed(VK_ESCAPE);
@@ -74,106 +75,103 @@ void getInput()
 
 void update(double dt)
 {
-
-
 	// get the delta time
 	elapsedTime += dt;
 	deltaTime = dt;
 
 	// Updating the location of the character based on the key press
+
 	if (keyPressed[K_UP] && charLocation.Y > 0)
 	{
 		Beep(1440, 30);
-		charLocation.Y--; 
+		charLocation.Y -= 2;
 	}
 	if (keyPressed[K_LEFT] && charLocation.X > 0)
 	{
 		Beep(1440, 30);
-		charLocation.X--; 
+		charLocation.X -= 2; 
 	}
-	if (keyPressed[K_DOWN] && charLocation.Y < consoleSize.Y - 1)
+	if (keyPressed[K_RIGHT] && charLocation.X < consoleSize.X - 35)
 	{
 		Beep(1440, 30);
-		charLocation.Y++; 
-	}
-	if (keyPressed[K_RIGHT] && charLocation.X < consoleSize.X - 1)
-	{
-		Beep(1440, 30);
-		charLocation.X++; 
-	}
-
-	// Shannon Touching an enemy results in losing life and removal of enemy
-	for (int i = 0; i < 3; ++i)
-	{
-		if (charLocation.X == enemyLocation[i].X && charLocation.Y == enemyLocation[i].Y)
-		{
-			Life.Value -= 1;
-			enemyLocation[i].X = 0;
-			enemyLocation[i].Y = 0;
-		}
+		charLocation.X += 2; 
 	}
 
 	// quits the game if player hits the escape key
 	if (keyPressed[K_ESCAPE])
-		g_quitGame = true; 
+		g_quitGame = true;    
+}
+
+void printBasket()
+{
+	gotoXY(charLocation.X, charLocation.Y-4);
+	colour(0x1A);
+	std::cout << "  .=========." << endl;
+
+	gotoXY(charLocation.X, charLocation.Y-3);
+	colour(0x1A);
+	std::cout << " l/        /l" << endl;
+
+	gotoXY(charLocation.X, charLocation.Y-2);
+	colour(0x1A);
+	std::cout << " l========lX:" << endl;
+
+	gotoXY(charLocation.X, charLocation.Y-1);
+	colour(0x1A);
+	std::cout << " lXXXXXXXXl/" << endl;
+	
+	gotoXY(charLocation);
+	colour(0x1A);
+	std::cout << " ---------- ";
 }
 
 void render()
 {
-
 	// clear previous screen
-	colour(0x0F);
+	colour(0x1A);
 	cls();
-
-	//render the game
-
-	//render test screen code (not efficient at all)
-	const WORD colors[] =   {
-		0x1A, 0x2B, 0x3C, 0x4D, 0x5E, 0x6F,
-		0xA1, 0xB2, 0xC3, 0xD4, 0xE5, 0xF6
-	};
-
-	for (int i = 0; i < 12; ++i)
-	{
-		gotoXY(3*i,i+1);
-		colour(colors[i]);
-		std::cout << "WOW";
-	}
 
 	// render time taken to calculate this frame
 	gotoXY(70, 0);
 	colour(0x1A);
-	std::cout << 1.0 / deltaTime << "fps" << std::endl;
+	std::cout << 1.0 / deltaTime << "f/s" << std::endl;
 
 	gotoXY(0, 0);
 	colour(0x59);
 	std::cout << elapsedTime << "secs" << std::endl;
-
+	
 	if(score == 1000000000)
 	{
 		gotoXY(50, 20);
 	    colour(0xF6);
 		std::cout << "Congrats!"<< "\nYou  won!";
 	}
+
 	else
 	{
-		gotoXY(69, 2);
+		gotoXY(62, 2);
 	    colour(0xF6);
 		std::cout << "High Score:\n";
-		gotoXY(69, 3);
+		gotoXY(62, 3);
 	    colour(0xF6);
 		std::cout <<score;
 	}
 
+	for (int y = 2; y <= 71; y++)
+	{
+	gotoXY(60,y);
+	colour(0x3A);
+	std::cout << 'x';
+	}
+
 	// render character
 	gotoXY(charLocation);
-	colour(0x0C);
-	std::cout << (char)1;
+	printBasket();
 
-	 // Shannon : Render Enemy
+	// Shannon : Render Enemy
 	for (int i = 0; i < 3; ++i)
 	{
-			gotoXY(enemyLocation[i]);
+			gotoXY(60,67);
 			colour(0x1A);
 			std::cout <<(char)1;
 	}
@@ -187,5 +185,5 @@ void render()
 			colour(0x0C);
 			std::cout <<(char)1;
 		}
-	}
+}
 }
